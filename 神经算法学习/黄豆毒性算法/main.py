3a1b915b4351274b4f88044b153505295d79a261
gongaustin <28402674@qq.com>
2021-12-21 12:30:13 +0800

    学习

--- 神经算法学习/黄豆毒性算法/main.py
+++ 神经算法学习/黄豆毒性算法/main.py
@@ -9,6 +9,25 @@ plt.title("Size Toxicity Function",fontsize=12)
 plt.xlabel("Bean Size")
 plt.ylabel("Toxicity")
 plt.scatter(xs,ys)
+
+
+
+w = 0.5
+alpha = 0.05 #学习率
+for i in range(100):
+    for i in range(100):
+        x = xs[i]
+        y = ys[i]
+        #学习值
+        y_pre = x * w
+        #误差
+        e = y - y_pre
+        w = w + alpha * x * e
+
+y_pre = xs*w
+
+plt.plot(xs,y_pre)
+
 plt.show()
 
 
