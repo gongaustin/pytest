109830ce670751156663b342eb24ce7b2f737b11
gongaustin <28402674@qq.com>
2021-12-21 12:40:08 +0800

    学习

--- 神经算法学习/黄豆毒性算法/main.py
+++ 神经算法学习/黄豆毒性算法/main.py
@@ -16,15 +16,23 @@ w = 0.5
 alpha = 0.05 #学习率
 for i in range(100):
     for i in range(100):
+        #自变量
         x = xs[i]
+        #因变量
         y = ys[i]
         #学习值
         y_pre = x * w
         #误差
         e = y - y_pre
+        #新的对应关系
         w = w + alpha * x * e
+#最后w肯定收敛
 
 y_pre = xs*w
+
+#打印y=wx的关系值
+print(w)
+
 
 plt.plot(xs,y_pre)
 
